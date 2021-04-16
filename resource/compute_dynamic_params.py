d63beccef8c79a0834a089cd584a7d2df4200817
Allan Francani <allan.francani@dataiku.com>
2021-04-16 11:13:34 +0200

    Refactoring
    [ch63042]

--- resource/compute_dynamic_params.py
+++ resource/compute_dynamic_params.py
@@ -1,32 +1,11 @@
 from dataiku import SQLExecutor2, api_client, default_project_key
 
 
-def qualify_name(row, quoted=True):
-    catalog_column_index = 2
-    schema_column_index = 3
-    object_column_index = 1
-    if quoted:
-        return f"\"{row[catalog_column_index]}\".\"{row[schema_column_index]}\".\"{row[object_column_index]}\""
-    else:
-        return f"{row[catalog_column_index]}.{row[schema_column_index]}.{row[object_column_index]}"
-
-
-def file_format_label(row):
-    qualified_file_format_name = f"{qualify_name(row, False)}"
-    comment = f"({row[6]})" if row[6] else ''
-    return f"{qualified_file_format_name} {comment}"
-
-
-def get_dss_dataset(dataset_name):
-    project_key = default_project_key()
-    project = api_client().get_project(project_key)
-    dss_dataset = project.get_dataset(dataset_name)
-    return dss_dataset
-
-
 def do(payload, config, plugin_config, inputs):
+    # if the macro is run from the flow, the input_dataset param is set...
     dataset_name = config.get('input_dataset')
 
+    # ...thus we know whether we should display the macro in "flow" or "scenario" style
     if dataset_name:
         return macro_from_dataset_params(payload['parameterName'], dataset_name)
     else:
@@ -34,54 +13,43 @@ def do(payload, config, plugin_config, inputs):
 
 
 def macro_from_scenario_params(parameter_name):
-    project_key = default_project_key()
-    project = api_client().get_project(project_key)
-    snowflake_datasets = filter(lambda dataset: dataset.type == 'Snowflake', project.list_datasets())
-
-    dataset_per_connection = dict()
-    for dataset in snowflake_datasets:
+    # We start by fetching all snowflake datasets in the current project and group them per connection
+    datasets_per_connection = dict()
+    for dataset in get_snowflake_datasets():
         connection = dataset['params']['connection']
-        if connection not in dataset_per_connection:
-            dataset_per_connection[connection] = []
-        dataset_per_connection[connection] += [dataset.name]
+        if connection not in datasets_per_connection:
+            datasets_per_connection[connection] = []
+        datasets_per_connection[connection] += [dataset.name]
 
-    multiple_connections = len(dataset_per_connection) > 1
+    # Only if there are multiple Snowflake connections, we need to display the different inputs grouped by connections
+    multiple_connections = len(datasets_per_connection) > 1
 
-    prefix = '⠀⠀' if multiple_connections else ''
+    # indent only if we have multiple Snowflake connections
+    indent = lambda choice: {'value': choice['value'], 'label': f"⠀⠀{choice['label']}"} if multiple_connections else lambda choice: choice
 
     if parameter_name == 'dataset':
         choices = []
-        for connection in dataset_per_connection:
+        for connection in datasets_per_connection:
             if multiple_connections:
-                choices += [{"value": "", "label": f"In connection {connection}:"}]
-            choices += [{"value": dataset, "label": f"{prefix}{dataset}"} for dataset in dataset_per_connection[connection]]
-
+                choices += [connection_choice(connection)]
+            choices += [indent(dataset_choice(dataset)) for dataset in datasets_per_connection[connection]]
         return {'choices': choices}
 
     if parameter_name == 'stage':
         choices = []
-        for connection in dataset_per_connection:
-            executor = SQLExecutor2(connection=connection)
-            stage_iter = executor.query_to_iter("SHOW STAGES")
+        for connection in datasets_per_connection:
             if multiple_connections:
-                choices += [{"value": "", "label": f"In connection {connection}:"}]
-            choices += [{"value": qualify_name(stage_tuple),
-                        "label": f"{prefix}{qualify_name(stage_tuple, False)}"} for stage_tuple in stage_iter.iter_tuples()]
+                choices += [connection_choice(connection)]
+            choices += [indent(snowflake_object_choice(row)) for row in get_stages(connection=connection)]
         return {"choices": choices}
 
     if parameter_name == 'file_format':
-        # Given that file formats are fully qualified, our "default" option won't override an actual file format
-        choices = [{"value": "default", "label": f"{prefix}DEFAULT"}]
-        for connection in dataset_per_connection:
-            executor = SQLExecutor2(connection=connection)
-            file_format_iter = executor.query_to_iter("SHOW FILE FORMATS IN ACCOUNT")
+        choices = [indent(default_format_choice)]
+        for connection in datasets_per_connection:
             if multiple_connections:
-                choices += [{"value": "", "label": f"In connection {connection}:"}]
-            choices += [{"value": qualify_name(file_format_tuple),
-                        "label": f"{prefix}{file_format_label(file_format_tuple)}"} for file_format_tuple in file_format_iter.iter_tuples()]
+                choices += [connection_choice(connection)]
+            choices += [indent(snowflake_object_choice(row)) for row in get_file_formats(connection=connection)]
         return {"choices": choices}
-
-    return {}
 
 
 def macro_from_dataset_params(parameter_name, dataset_name):
@@ -89,27 +57,74 @@ def macro_from_dataset_params(parameter_name, dataset_name):
         return {"choices": [{"value": "default", "label": dataset_name}]}
 
     if parameter_name == 'stage':
-        dss_dataset = get_dss_dataset(dataset_name)
+        if not is_dataset_valid(dataset_name):
+            return {"choices": [invalid_dataset_choice]}
 
-        if dss_dataset.get_settings().type != 'Snowflake':
-            return {"choices": [{"value": None, "label": "⚠️ Invalid input dataset"}]}
-
-        executor = SQLExecutor2(dataset=dataset_name)
-        stage_iter = executor.query_to_iter("SHOW STAGES")
-        choices = [{"value": qualify_name(stage_tuple),
-                    "label": qualify_name(stage_tuple, False)} for stage_tuple in stage_iter.iter_tuples()]
+        choices = [snowflake_object_choice(row) for row in get_stages(dataset=dataset_name)]
         return {"choices": choices}
 
     if parameter_name == 'file_format':
-        dss_dataset = get_dss_dataset(dataset_name)
+        if not is_dataset_valid(dataset_name):
+            return {"choices": [invalid_dataset_choice]}
 
-        if dss_dataset.get_settings().type != 'Snowflake':
-            return {"choices": [{"value": None, "label": "⚠️ Invalid input dataset"}]}
-
-        executor = SQLExecutor2(dataset=dataset_name)
-        file_format_iter = executor.query_to_iter("SHOW FILE FORMATS IN ACCOUNT")
-        # Given that file formats are fully qualified, our "default" option won't override an actual file format
-        choices = [{"value": "default", "label": "DEFAULT"}] + \
-                  [{"value": qualify_name(file_format_tuple),
-                    "label": file_format_label(file_format_tuple)} for file_format_tuple in file_format_iter.iter_tuples()]
+        choices = [default_format_choice] + \
+                  [snowflake_object_choice(row) for row in get_file_formats(dataset=dataset_name)]
         return {"choices": choices}
+
+
+def get_snowflake_datasets():
+    project_key = default_project_key()
+    project = api_client().get_project(project_key)
+    return filter(lambda dataset: dataset.type == 'Snowflake', project.list_datasets())
+
+
+def is_dataset_valid(dataset_name):
+    project_key = default_project_key()
+    project = api_client().get_project(project_key)
+    dss_dataset = project.get_dataset(dataset_name)
+    return dss_dataset.get_settings().type == 'Snowflake'
+
+
+def get_stages(**kwargs):
+    return SQLExecutor2(**kwargs).query_to_iter("SHOW STAGES").iter_tuples()
+
+
+def get_file_formats(**kwargs):
+    return SQLExecutor2(**kwargs).query_to_iter("SHOW FILE FORMATS IN ACCOUNT").iter_tuples()
+
+
+def connection_choice(connection):
+    return {
+        "value": None,
+        "label": f"From connection {connection}:"
+    }
+
+
+# Given that file formats are fully qualified, our "default" option won't override an actual file format
+default_format_choice = {
+    "value": "default",
+    "label": "DEFAULT"
+}
+
+
+invalid_dataset_choice = {
+    "value": None,
+    "label": "⚠️ Invalid input dataset"
+}
+
+
+def dataset_choice(dataset_name):
+    return {
+        "value": dataset_name,
+        "label": dataset_name
+    }
+
+
+def snowflake_object_choice(row):
+    catalog = row[2]
+    schema = row[3]
+    name = row[1]
+    return {
+        "value": f"\"{catalog}\".\"{schema}\".\"{name}\"",
+        "label": f"{catalog}.{schema}.{name}"
+    }
